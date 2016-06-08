97bba0eef106f46ac439772969a940c7d755f261
Seray Uzgur <seray.uzgur@mebitech.com>
2016-06-08 12:26:18 +0300

    ESDoc

--- src/components/ShallowComponent.jsx
+++ src/components/ShallowComponent.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import shallowCompare from "react-addons-shallow-compare";
 
+/**
+ * A component which extends React.Component and implements shouldComponentUpdate with using shallowCompare.
+ */
 export default class ShallowComponent extends React.Component {
     render() : string {
         return this.props.children;
