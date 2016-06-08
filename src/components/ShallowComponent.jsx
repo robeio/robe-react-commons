52018bc079757c885a40407442a7c63b6dc6d639
Seray Uzgur <seray.uzgur@mebitech.com>
2016-06-08 12:03:52 +0300

    changed to export default.

--- src/components/ShallowComponent.jsx
+++ src/components/ShallowComponent.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import shallowCompare from "react-addons-shallow-compare";
 
-
-class ShallowComponent extends React.Component {
+export default class ShallowComponent extends React.Component {
     render() : string {
         return this.props.children;
     }
@@ -11,5 +10,3 @@ class ShallowComponent extends React.Component {
         return shallowCompare(this, nextProps, nextState);
     }
 }
-
-module.exports = ShallowComponent;
