86466fd7444802a087ac25de45b29de440fbe4cf
Kamil Büküm <kamil.bukum@mebitech.com>
2016-06-12 20:43:39 +0300

    disabled eslint no-useless-constructor

--- src/components/ShallowComponent.jsx
+++ src/components/ShallowComponent.jsx
@@ -1,3 +1,4 @@
+/* eslint no-useless-constructor: 0*/
 import React from "react";
 import shallowCompare from "react-addons-shallow-compare";
 
@@ -5,10 +6,14 @@ import shallowCompare from "react-addons-shallow-compare";
  * A component which extends React.Component and implements shouldComponentUpdate with using shallowCompare.
  */
 export default class ShallowComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
     render() : string {
         return this.props.children;
     }
-
     shouldComponentUpdate(nextProps : Object, nextState : Object) : boolean {
         return shallowCompare(this, nextProps, nextState);
     }
