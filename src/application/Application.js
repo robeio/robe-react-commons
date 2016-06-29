84350b40f4de5f0fd3225d4a59550f59ac451a5e
Kamil Büküm <kamil.bukum@mebitech.com>
2016-06-29 15:08:40 +0300

    - added getUrl method to the Application.

--- src/application/Application.js
+++ src/application/Application.js
@@ -18,14 +18,22 @@ class Application {
     setBaseUrlPath = (value): boolean => {
         Assertions.isUrl(value, true);
         value = value.trim();
-        if (value.endsWith("/")) {
-            value = `${value}/`;
-        }
         this.props.set(this._baseUrlPathKey, value);
     }
 
     getBaseUrlPath = () : any => {
         return this.getProps().get(this._baseUrlPathKey);
+    }
+
+    getUrl = (url: string): string => {
+        Assertions.isNotEmpty(url, true);
+        if (!Assertions.isUrl(url) && Assertions.isNotEmpty(this.getBaseUrlPath())){
+            if (!url.startsWith("/")) {
+                url = `/${url}`;
+            }
+            return this.getBaseUrlPath() + url;
+        }
+        return url;
     }
 }
 
