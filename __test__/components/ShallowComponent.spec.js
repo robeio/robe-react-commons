03f4d7a04ca059d2c44006d0da0da894f8937c1a
serayuzgur <serayuzgur@gmail.com>
2016-07-01 00:35:03 +0300

    ShallowComponent tests

--- __test__/components/ShallowComponent.spec.js
+++ __test__/components/ShallowComponent.spec.js
@@ -1,5 +1,52 @@
-describe("Application.js", () => {
-    it("constructors", () => {
+import React from "react";
+import ReactDOM from "react-dom";
+import TestUtils from "react-addons-test-utils";
+import chai from "chai";
+import ShallowComponent from "components/ShallowComponent";
+import Maps from "utils/Maps";
 
+describe("ShallowComponent.js", () => {
+
+    class TestComponent extends ShallowComponent {
+        constructor(props) {
+            super(props);
+            this.state = { test: 0 };
+        }
+        render() {
+            return (
+                <div>{this.state.test}</div>
+            );
+        }
+    }
+    const shallow = (
+        <ShallowComponent>
+            <div>ShallowComponent</div>
+        </ShallowComponent>
+    );
+    const test = (
+        <TestComponent />
+    );
+
+    let renderedItem = TestUtils.renderIntoDocument(shallow);
+
+    it("getName", () => {
+        chai.assert.equal(renderedItem.getName(), "ShallowComponent");
+    });
+
+    it("render", () => {
+        let child = TestUtils.findRenderedDOMComponentWithTag(renderedItem, "div");
+        chai.assert.equal(child.innerHTML, "ShallowComponent");
+    });
+
+    it("shouldComponentUpdate", () => {
+        renderedItem = TestUtils.renderIntoDocument(test);
+        let child = TestUtils.findRenderedDOMComponentWithTag(renderedItem, "div");
+        chai.assert.equal(child.innerHTML, "0");
+
+        let state = { test: 1 };
+        renderedItem.setState(state);
+
+        child = TestUtils.findRenderedDOMComponentWithTag(renderedItem, "div");
+        chai.assert.equal(child.innerHTML, "1");
     });
 });
