11b0fb15233a7933c788a5b638d6e4ac0d8f8604
Kamil Büküm <kamil.bukum@mebitech.com>
2016-07-06 14:02:17 +0300

    -  added some code to the LocalEndPoint but not finished yet.

--- src/endpoint/LocalEndPoint.js
+++ src/endpoint/LocalEndPoint.js
@@ -1,18 +1,128 @@
+import ErrorUtility from "../utils/ErrorUtility";
+import { Criteria, Restrictions } from "js-criteria/lib";
+
 export default class LocalEndPoint {
+    __data;
 
-    read(_offset, _limit, _query, code, value, fields, successCallBack, errorCallback) {
-        this.__notImplementedError("read");
+    constructor(props: Object) {
+        this.__data = props.data;
     }
 
-    create(item: Map, successCallback, errorCallback) {
-        this.__notImplementedError("create");
+    _query = (query) => {
+        let restrictions = [];
+        for (let i = 0; i < query.length; i++) {
+            let filter = query[i];
+            if (filter) {
+                let restriction;
+                switch (filter.operator) {
+                    case "and" :
+                        restriction = Restrictions.and.apply(null, this._query(filter.functions));
+                        break;
+                    case "or" :
+                        restriction = Restrictions.and.apply(null, this._query(filter.functions));
+                        break;
+                    case "=" :
+                    case "!=" :
+                    case "<" :
+                    case "<=" :
+                    case ">" :
+                    case ">=" :
+                        restriction = Restrictions.op(filter.operator, filter.key, filter.value);
+                        break;
+                    case "~=" :
+                        restriction = Restrictions.ilike(filter.key, `%${filter.value}%`);
+                        break;
+                    case "|=" :
+                        restriction = Restrictions.in(filter.key, filter.values);
+                        break;
+                    default :
+                        if (filter.function) {
+                            restriction = filter.function;
+                        }
+                }
+                restrictions.push(restriction);
+            }
+        }
+        return restrictions;
+    }
+    read(offset: string, limit: string, query: string, filter: string, fields: string, successCallBack: Function, errorCallback: Function): boolean {
+        let criteria = new Criteria(this.__data);
+
+        if (offset) {
+            criteria.setFirstResult(offset);
+        }
+        if (limit) {
+            criteria.setMaxResults(limit);
+        }
+        if (query) {
+            for (let i = 0; i < query.length; i++) {
+                let filter = query[i];
+
+            }
+        }
+
+
+        let queryParams = {
+            offset: offset,
+            limit: limit,
+            query: query,
+            filter: filter,
+            fields: fields
+        };
+
+        let onSuccess = (data: Object, textStatus: string, xhr: Object) => {
+            let result = {
+                data: data,
+                totalCount: parseInt(xhr.getResponseHeader("X-Total-Count"), 10) || 0
+            };
+            successCallBack(result);
+        };
+
+        return this._readRequest.call(undefined, queryParams, onSuccess, this.__createOnError(errorCallback));
     }
 
-    update(oldItem:Map, newItem:Map, successCallback, errorCallback) {
-        this.__notImplementedError("update");
+    create(item: Map, successCallback: Function, errorCallback: Function): boolean {
+        let onSuccess = (data: Object) => {
+            let result = {
+                data: data,
+                totalCount: 1
+            };
+            successCallback(result);
+        };
+        return this._createRequest.call(item, undefined, onSuccess, this.__createOnError(errorCallback));
     }
 
-    delete(item:Map, successCallback, errorCallback) {
-        this.__notImplementedError("delete");
+    update(newItem: Map, idField, successCallback: Function, errorCallback: Function) {
+        let onSuccess = (data: Object) => {
+            let result = {
+                data: data,
+                totalCount: 1
+            };
+            successCallback(result);
+        };
+
+        this._updateRequest.call(newItem, undefined, onSuccess, this.__createOnError(errorCallback), [newItem[idField]]);
+    }
+
+    delete(item: Map, idField, successCallback: Function, errorCallback: Function) {
+        let onSuccess = (data: Object) => {
+            let result = {
+                data: data,
+                totalCount: 1
+            };
+            successCallback(result);
+        };
+        this._deleteRequest.call(item, undefined, onSuccess, this.__createOnError(errorCallback), [item[idField]]);
+    }
+
+    getUrl(): string {
+        return this.__url;
+    }
+
+    __createOnError(errorCallback: Function): Function {
+        return (xhr: Object, exception: string) => {
+            let error = ErrorUtility.parseHttpError(xhr, exception);
+            errorCallback(error);
+        };
     }
 }
