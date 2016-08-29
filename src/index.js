97221d2c904b94d5a4b928d7058fb4f92eadd198
Kamil Büküm <kamil.bukum@mebitech.com>
2016-08-29 16:32:14 +0300

    - added utils/Strings to index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ export { default as StoreComponent } from "./components/StoreComponent";
 
 
 export { default as Arrays } from "./utils/Arrays";
-export { default as Objects } from "./utils/Objects";
-export { default as Maps } from "./utils/Maps";
-export { default as Cookies } from "./utils/Cookies";
 export { default as Assertions } from "./utils/Assertions";
+export { default as Cookies } from "./utils/Cookies";
+export { default as Generator } from "./utils/Generator";
+export { default as Maps } from "./utils/Maps";
+export { default as Objects } from "./utils/Objects";
+export { default as Strings } from "./utils/Strings";
 
 export { default as MapArray } from "./collections/MapArray";
 
@@ -21,5 +23,3 @@ export { default as Store } from "./stores/Store";
 
 export { default as LocalEndPoint } from "./endpoint/LocalEndPoint";
 export { default as RemoteEndPoint } from "./endpoint/RemoteEndPoint";
-
-export { default as Generator } from "./utils/Generator";
